7d7c70089f2d46bd0cc70808beaae65caa0d17f2
droppedyourdingo <huynhdt94@gmail.com>
2023-08-01 14:29:23 -0400

    renders player names

--- src/API/ajaxHelpers.js
+++ src/API/ajaxHelpers.js
@@ -1,9 +1,20 @@
 const API = `https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-B`
 
 
-export default function fetchPlayers () {
+export async function fetchPlayers () {
+        try {
+            const response = await fetch(`${API}/players/`,{
+                method: 'GET',
+                headers: { "Content-Type": "application/json"},
+                
+            });
+            const result = await response.json();
+            
+            
 
-    return (
-        console.log("placeholder")
-    )
-}
+            return result.data.players;
+            
+        } catch (err) {
+            console.error('Uh oh, trouble fetching players!', err);
+        }
+    }
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,8 +1,43 @@
+import { fetchPlayers } from "../API/ajaxHelpers"
+import {useState, useEffect } from "react"
 
 
 export default function AllPlayers() {
 
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const playerData = await fetchPlayers();
+                 
+                setPlayers(playerData);
+            }
+
+            catch(err) {
+                console.error('Uh oh, trouble fetching players!', err)
+            }
+
+            
+        };
+
+        fetchData();
+
+    },[])
+
+
+
     return(
-        <h1>allplayers component</h1>
+        <div>
+            <h1>All Players</h1>
+            <ul>
+            {players.map((player) => (
+                
+                <li key={player.id}>{player.name}</li>
+
+            ))}
+            </ul>
+        </div>
     )
 }
